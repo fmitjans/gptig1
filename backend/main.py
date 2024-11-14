c631c9409ab29614f5e88ae3127cb3f9fa23a715
fran-gl <frangl@uc.cl>
2024-11-14 15:09:43 -0300

    requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 import openai
 
 load_dotenv()
-#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def init_driver(usar_firefox=False):
