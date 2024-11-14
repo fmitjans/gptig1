feb6055722f0481da13728f5dc69be1ccd2c6def
fran-gl <frangl@uc.cl>
2024-11-14 20:31:08 -0300

    change parameter

--- backend/parameters.py
+++ backend/parameters.py
@@ -1,3 +1,3 @@
-USING_FIREFOX = True # C para chrome, F para Firefox
+USING_FIREFOX = False # C para chrome, F para Firefox
 GECKODRIVER_LOCATION="/usr/local/bin/geckodriver"
 # GECKODRIVER_LOCATION="/snap/bin/firefox.geckodriver"
